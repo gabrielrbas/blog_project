5755afc6537dd651d24da38b2c9bc2a4a818714f
Gabriel Ribas <gabrielr.bas@gmail.com>
2021-05-30 20:42:44 -0300

    updated posts views.py

--- posts/views.py
+++ posts/views.py
@@ -2,12 +2,26 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
 from .models import Post
+from django.db.models import Q, Count, Case, When
 
 
 class PostIndex(ListView):
     model = Post
     template_name = 'posts/index.html'
+    paginate_by = 6
     context_object_name = 'posts'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = qs.order_by('-id')
+        return qs
+        qs = qs.annotate(
+            numero_comentario=Count(
+                Case(
+                    When(comentario__publicado_comentario=True, then=1)
+                )
+            )
+        )
 
 
 class PostBusca(PostIndex):
