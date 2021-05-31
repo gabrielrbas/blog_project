79f239a444b5f85d178b48aaab8b65dc7b1a8815
Gabriel Ribas <gabrielr.bas@gmail.com>
2021-05-30 21:29:49 -0300

    updated posts views.py

--- posts/views.py
+++ posts/views.py
@@ -31,6 +31,15 @@ class PostBusca(PostIndex):
 class PostCategoria(PostIndex):
     template_name = 'posts/post_categoria.html'
 
+    def get_queryset(self):
+        qs = super().get_queryset()
+        categoria = self.kwargs.get('categoria', None)
+        if not categoria:
+            return qs
+        qs = qs.filter(categoria_post__nome_cat__iexact=categoria)
+
+        return qs
+
 
 class PostDetalhes(UpdateView):
     pass
